d1bc7d57325bbdcffd723e5ed12aa911e6173f5e
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-06-24 17:53:10 +0200

    Data Model added

--- iMap/Models/Location.swift
+++ iMap/Models/Location.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
+
+
+
