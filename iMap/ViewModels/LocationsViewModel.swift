79ca51e9e4b11f4f774bfe17c3a3eab3f9a22947
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-06-24 20:12:31 +0200

    [ViewModel] Updated ViewModel with 2 functions based on LocationList

--- iMap/ViewModels/LocationsViewModel.swift
+++ iMap/ViewModels/LocationsViewModel.swift
@@ -21,8 +21,12 @@ class LocationsViewModel: ObservableObject {
         }
     }
     
+    // Current region on map
     @Published var mapRegion: MapCameraPosition
     let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    // Show list of locations
+    @Published var showLocationsList: Bool = false
     
     init() {
         let locations = LocationsDataService.locations
@@ -40,4 +44,16 @@ class LocationsViewModel: ObservableObject {
         }
     }
     
+    func toggleLocationsList() {
+        withAnimation(.easeInOut){
+            showLocationsList.toggle()
+        }
+    }
+    
+    func showNextLocation(location: Location) {
+        withAnimation(.easeInOut) {
+            mapLocation = location
+            showLocationsList = false
+        }
+    }
 }
