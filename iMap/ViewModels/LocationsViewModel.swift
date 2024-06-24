3bdc24d39ab421a475860458010e3b05acb4ae6c
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-06-24 18:13:13 +0200

    [App Building] Created ViewModel

---
+++ iMap/ViewModels/LocationsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationsViewModel.swift
+//  iMap
+//
+//  Created by admin on 24/06/2024.
+//
+
+import Foundation
---
+++ iMap/Views/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  iMap
+//
+//  Created by admin on 24/06/2024.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
