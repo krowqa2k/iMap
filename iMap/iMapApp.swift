81e4008f6bdabf0464fb8ce3a468f7b7dea2dc8f
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-06-24 17:40:55 +0200

    Initial Commit

---
+++ iMap/iMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMapApp.swift
+//  iMap
+//
+//  Created by admin on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct iMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
