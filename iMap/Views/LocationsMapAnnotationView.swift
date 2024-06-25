78c28d48859936ae8461b195520ea5db65da9dc8
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-06-25 17:47:20 +0200

    [App Progress] Added new AnnotationView

---
+++ iMap/Views/LocationsMapAnnotationView.swift
@@ -0,0 +1,40 @@
+//
+//  LocationsMapAnnotationView.swift
+//  iMap
+//
+//  Created by admin on 25/06/2024.
+//
+
+import SwiftUI
+
+struct LocationsMapAnnotationView: View {
+    
+    let accentColor = Color("AccentColor")
+    
+    var body: some View {
+        VStack(spacing: 0){
+            Image(systemName: "map.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .font(.headline)
+                .foregroundStyle(.white)
+                .padding(6)
+                .background(accentColor)
+                .cornerRadius(36)
+            
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(accentColor)
+                .frame(width: 10, height: 10)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -2)
+                .padding(.bottom, 40)
+        }
+    }
+}
+
+#Preview {
+    LocationsMapAnnotationView()
+}
